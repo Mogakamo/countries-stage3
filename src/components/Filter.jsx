19547d76a44f4881fbee377a03d4974e80ced5c3
Mogakamo <mogaka.amo254@gmail.com>
2022-11-11 14:51:31 +0300

    [UI]: finished with the filter section

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { FunnelIcon, GlobeAltIcon } from "react-native-heroicons/outline";
 
 const Filter = () => {
   return (
-    <View>
-      <Text>Filter</Text>
+    <View className="flex-row justify-between">
+      <View className="flex-row items-center p-2 border-gray-500/10 rounded-md h-auto shadow-lg border-2 w-auto">
+        <GlobeAltIcon color="grey" size={20} />
+        <Text className="text-xs">EN</Text>
+      </View>
+      <View className="flex-row items-center border-gray-500/10 rounded-md border-2 p-2">
+        <FunnelIcon color="grey" size={20} />
+        <Text className="text-xs">Filter</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
