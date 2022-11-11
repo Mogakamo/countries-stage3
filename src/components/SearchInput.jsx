7049e3592c58a03d2f3ba140f6b9145a4eee5e7a
Mogakamo <mogaka.amo254@gmail.com>
2022-11-11 17:05:06 +0300

    [ui]: countries are now in the app and you can view it

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,7 +3,7 @@ import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
 const SearchInput = () => {
   return (
-    <View className="py-2 px-5 ">
+    <View className="py-2 px-5 my-4">
       <View className="p-2 items-center flex-row border-transparent bg-gray-400/40 space-x-3 border-gray-500 rounded-lg">
         <MagnifyingGlassIcon color="grey" fill="transparent" size={21} />
         <TextInput className="outline-none w-full h-full placeholder:text-lg placeholder:pl-16" placeholder="Search Country" />
--- src/screens/CountriesScreen.jsx
+++ src/screens/CountriesScreen.jsx
@@ -1,14 +1,50 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyledText,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import SearchInput from "../components/SearchInput";
 import Filter from "../components/Filter";
+import axios from "axios";
 
 const CountriesScreen = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const data = axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((data) => {
+        setCountries(data.data);
+      });
+  }, []);
+
   return (
     <View>
       <SearchInput />
       <View className="px-5">
         <Filter />
+        <ScrollView className="mt-5 space-y-4" showsVerticalScrollIndicator={true}>
+          {countries
+            .sort()
+            .map((country, id) => (
+              <View key={id} className="flex-row">
+                <Image
+                  source={{ uri: country.flags.png }}
+                  className="h-12 w-12 border-2 border-black rounded-lg"
+                  // style={{ with: 200, height: 200 }}
+                />
+                <View className="flex-col ml-3">
+                  <Text>{country.name.common}</Text>
+                  <Text>{country.name.official}</Text>
+                </View>
+              </View>
+            ))}
+        </ScrollView>
       </View>
     </View>
   );
